008a4e3f2ac53c7bf82d71344cb5ce68e2059afa
saloev <saloev.saadi@yandex.ru>
2019-05-11 13:01:02 +0300

    configure vue for laravel and endpoint grpahql

--- vue.config.js
+++ vue.config.js
@@ -11,21 +11,23 @@ module.exports = {
     },
   },
 
+   // proxy API requests to Valet during development
   devServer: {
+    proxy: 'http://127.0.0.1:8000',
     watchOptions: {
       poll: true
     }
   },
 
-  // chainWebpack: (config) => {
-  //   const svgRule = config.module.rule('svg');
+  // output built static files to Laravel's public dir.
+  // note the "build" script in package.json needs to be modified as well.
+  outputDir: '../public',
 
-  //   svgRule.uses.clear();
-
-  //   svgRule
-  //     .use('vue-svg-loader')
-  //     .loader('vue-svg-loader');
-  // },
+  // modify the location of the generated HTML file.
+  // make sure to do this only in production.
+  indexPath: process.env.NODE_ENV === 'production'
+    ? '../resources/views/index.blade.php'
+    : 'index.html',
 
   configureWebpack: {
    devtool: 'source-map'
